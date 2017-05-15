a36444f3861e000e8cdc71cf973d390acfbe8d38
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-15 17:33:21 +0300

    update version version=MinoR

--- version.js
+++ version.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec;
 
 exec("git log -1 --pretty=format:%s", function(error, stdout, stderr) {
   const regRes = /version=(major|minor|patch)/ig.exec(stdout);
-  const version = (regRes && regRes[1]) || 'patch';
+  const version = (regRes && regRes[1].toLowerCase()) || 'patch';
 
   console.log(version)
 });
