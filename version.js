45c34ef7592a7fd51e31c1bf6fb0613ffedf5762
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-15 17:43:29 +0300

    update version.js [minor]

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 
 exec("git log -1 --pretty=format:%s", function(error, stdout, stderr) {
-  const regRes = /version=(major|minor|patch)/ig.exec(stdout);
+  const regRes = /\[(major|minor|patch)\[/ig.exec(stdout);
   const version = (regRes && regRes[1].toLowerCase()) || 'patch';
 
   console.log(version)
